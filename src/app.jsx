ad7e924b4df21d755ea28b62e1856a056736562a
Chris Tso <christophertso2017@u.northwestern.edu>
2019-05-11 16:26:39 -0500

    Added display of top scores on load and after posting scores

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppStyle from './app.css';
+import axios from 'axios';
 
 class App extends React.Component{
     constructor(props){
@@ -9,12 +10,17 @@ class App extends React.Component{
             score : 0, 
             choice: 0, 
             timeout: null,
+            leaders : null
         }
     this.start = this.start.bind(this);   
     this.changeTimeAndChoice = this.changeTimeAndChoice.bind(this); 
     this.addScore = this.addScore.bind(this);
+    this.getScores = this.getScores.bind(this);
     }
 
+    componentDidMount(){
+        this.getScores();
+    }
     start(){
         var timeout = setTimeout(this.changeTimeAndChoice, 1000);
         this.setState({timeout : timeout});
@@ -29,11 +35,25 @@ class App extends React.Component{
         this.setState({timeout : timeout2});
         if(this.state.timeout === 61){
             clearTimeout(timeout2);
+            axios.post('/addToScore',
+                {
+                  score: this.state.score  
+                })
+                .then(this.getScores)
+                .catch((err) => {console.log(err)})
         }
     }
     addScore(){
         var score = this.state.score; 
         this.setState({score: score + 1});
+    }
+    getScores(){
+        axios.get('/getScores')
+        .then((data) => {
+            var topThree = [data.data[0].score, data.data[1].score, data.data[2].score];
+            this.setState({leaders: topThree});
+        })
+        .catch((err) => {console.log(err)});
     }
     render(){
         return (
@@ -53,6 +73,9 @@ class App extends React.Component{
               </div>
               <div className = {AppStyle.leaders}>
                 High Scores
+                {this.state.leaders !== null && <div>1. {this.state.leaders[0]} </div> }
+                {this.state.leaders !== null && <div>2. {this.state.leaders[1]} </div> }
+                {this.state.leaders !== null && <div>3. {this.state.leaders[2]} </div> }
               </div>
             </div>
 
